af4a514579e8f68b6c9f5cc713762df3401c9b99
jonathanalmoradie <jonathan.almoradie@gmail.com>
2023-07-15 14:18:19 -0700

    Week5Homework submission

---
+++ Week5-Homework/Array-Methods-Practice/bonusChallenge.js
@@ -0,0 +1,410 @@
+const data = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "et porro tempora",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: "qui ullam ratione quibusdam voluptatem quia omnis",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: "illo expedita consequatur quia in",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: "quo adipisci enim quam ut ab",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: "molestiae perspiciatis ipsa",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: "illo est ratione doloremque quia maiores aut",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 11,
+    title: "vero rerum temporibus dolor",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 12,
+    title: "ipsa repellendus fugit nisi",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 13,
+    title: "et doloremque nulla",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 14,
+    title: "repellendus sunt dolores architecto voluptatum",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 15,
+    title: "ab voluptatum amet voluptas",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 16,
+    title: "accusamus eos facilis sint et aut voluptatem",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 17,
+    title: "quo laboriosam deleniti aut qui",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 18,
+    title: "dolorum est consequatur ea mollitia in culpa",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 19,
+    title: "molestiae ipsa aut voluptatibus pariatur dolor nihil",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 20,
+    title: "ullam nobis libero sapiente ad optio sint",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 21,
+    title: "suscipit repellat esse quibusdam voluptatem incidunt",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 22,
+    title: "distinctio vitae autem nihil ut molestias quo",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 23,
+    title: "et itaque necessitatibus maxime molestiae qui quas velit",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 24,
+    title: "adipisci non ad dicta qui amet quaerat doloribus ea",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 25,
+    title: "voluptas quo tenetur perspiciatis explicabo natus",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 26,
+    title: "aliquam aut quasi",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 27,
+    title: "veritatis pariatur delectus",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 28,
+    title: "nesciunt totam sit blanditiis sit",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 29,
+    title: "laborum aut in quam",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 30,
+    title:
+      "nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 31,
+    title: "repudiandae totam in est sint facere fuga",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 32,
+    title: "earum doloribus ea doloremque quis",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 33,
+    title: "sint sit aut vero",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 34,
+    title: "porro aut necessitatibus eaque distinctio",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 35,
+    title: "repellendus veritatis molestias dicta incidunt",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 36,
+    title: "excepturi deleniti adipisci voluptatem et neque optio illum ad",
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 37,
+    title: "sunt cum tempora",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 38,
+    title: "totam quia non",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 39,
+    title: "doloremque quibusdam asperiores libero corrupti illum qui omnis",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 40,
+    title: "totam atque quo nesciunt",
+    completed: true
+  },
+  {
+    userId: 3,
+    id: 41,
+    title:
+      "aliquid amet impedit consequatur aspernatur placeat eaque fugiat suscipit",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 42,
+    title: "rerum perferendis error quia ut eveniet",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 43,
+    title: "tempore ut sint quis recusandae",
+    completed: true
+  },
+  {
+    userId: 3,
+    id: 44,
+    title: "cum debitis quis accusamus doloremque ipsa natus sapiente omnis",
+    completed: true
+  },
+  {
+    userId: 3,
+    id: 45,
+    title: "velit soluta adipisci molestias reiciendis harum",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 46,
+    title: "vel voluptatem repellat nihil placeat corporis",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 47,
+    title: "nam qui rerum fugiat accusamus",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 48,
+    title: "sit reprehenderit omnis quia",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 49,
+    title: "ut necessitatibus aut maiores debitis officia blanditiis velit et",
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 50,
+    title: "cupiditate necessitatibus ullam aut quis dolor voluptate",
+    completed: true
+  }
+];
+
+// create an object that summarizes the incomplete tasks of users 3, 7 & 8
+// ^^^ Only has users 1, 2 & 3
+
+// example output:
+// {
+//   user3: {
+//     totalTasks: x,
+//     totalIncomplete: y,
+//     incompleteTasks: [title1, title2, title3, ...]
+//   },
+//   user7: ...
+// }
+
+
+// users obj. structure
+const users = {
+  user1: {
+    totalTasks: 0,
+    totalIncomplete: 0,
+    incompleteTasks: []
+  },
+  user3: {
+    totalTasks: 0,
+    totalIncomplete: 0,
+    incompleteTasks: []
+  }
+};
+// ------
+
+// User1
+users.user1.totalTasks = data.reduce((sum, task) => {
+  if (task.userId == 1) {
+    sum++;
+  }
+  return sum;
+}, 0);
+
+users.user1.totalIncomplete = data.reduce((sum, task) => {
+  if (task.userId == 1 && task.completed == false) {
+    sum++;
+  }
+  return sum;
+}, 0);
+
+data.forEach(task => {
+  if (task.userId == 1 && task.completed == false) {
+    users.user1.incompleteTasks.push(task.title);
+  }
+});
+// ------
+
+// User3
+users.user3.totalTasks = data.reduce((sum, tasks) => {
+  if (tasks.userId == 3) {
+    sum++;
+  }
+  return sum;
+}, 0);
+
+users.user3.totalIncomplete = data.reduce((sum, task) => {
+  if (task.userId == 3 && task.completed == false) {
+    sum++;
+  }
+  return sum;
+}, 0);
+
+data.forEach(task => {
+  if (task.userId == 3 && task.completed == false) {
+    users.user3.incompleteTasks.push(task.title);
+  }
+});
+// -------
+
+console.log(users);
+// print user list:
+// {
+//   user1: {
+//     totalTasks: 20,
+//     totalIncomplete: 9,
+//     incompleteTasks: [
+//       'delectus aut autem',
+//       'quis ut nam facilis et officia qui',
+//       'fugiat veniam minus',
+//       'laboriosam mollitia et enim quasi adipisci quia provident illum',
+//       'qui ullam ratione quibusdam voluptatem quia omnis',
+//       'illo expedita consequatur quia in',
+//       'molestiae perspiciatis ipsa',
+//       'et doloremque nulla',
+//       'dolorum est consequatur ea mollitia in culpa'
+//     ]
+//   },
+//   user3: {
+//     totalTasks: 10,
+//     totalIncomplete: 7,
+//     incompleteTasks: [
+//       'aliquid amet impedit consequatur aspernatur placeat eaque fugiat suscipit',
+//       'rerum perferendis error quia ut eveniet',
+//       'velit soluta adipisci molestias reiciendis harum',
+//       'vel voluptatem repellat nihil placeat corporis',
+//       'nam qui rerum fugiat accusamus',
+//       'sit reprehenderit omnis quia',
+//       'ut necessitatibus aut maiores debitis officia blanditiis velit et'
+//     ]
+//   }
+// }
